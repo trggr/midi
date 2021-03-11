5d3c5154da9dcb7de6d446f88c77998dd8fc6f19
Tim Ashchepkov <tashepkov@gmail.com>
2021-03-11 17:40:01 -0500

    Saved 5 songs

--- src/midi/dbload.clj
+++ src/midi/dbload.clj
@@ -47,7 +47,6 @@
                     "Fm7    | Bbm7     | Eb7    | Abmaj7   |"
                     "Dbmaj7 | Gb7      | Cm7    | Bdim7    |"
                     "Bbm7   | Eb7      | Abmaj7 | Gm7-5  C9 "))}
-
   {:id 2, :nm "In a Sentimental Mood", :numer 4, :denom 4, :ppq 400000, :bb 8, :bpm 120,
    :bars (bars (str "Dm       Dmmaj7 | Dm7    Dm6 | Gm      Gmmaj7 | Gm7      Gm6 A7   |"
                     "Dm              | D7         | Gm7     Gb7    | Fmaj7             |"
@@ -56,9 +55,39 @@
                     "Dbmaj7   Bbm7   | Ebm7   Ab7 | Dbmaj7  Bb7    | Eb7      Ab7      |"
                     "Dbmaj7   Bbm7   | Ebm7   Ab7 | Gm7            | C7                |"
                     "Dm       Dmmaj7 | Dm7    Dm6 | Gm      Gmmaj7 | Gm7      Gm6 A7   |"
-                    "Dm              | D7         | Gm7     C11-9  | Fmaj7              "))}])
+                    "Dm              | D7         | Gm7     C11-9  | Fmaj7              "))}
+  {:id 3, :nm "All of Me", :numer 4, :denom 4, :ppq 400000, :bb 8, :bpm 120,
+   :bars (bars (str "C6        |          | E7             |           |"
+                    "A7        |          | Dm7            |           |"
+                    "E7        |          | Am7            |           |"
+                    "D7        |          | Dm7            | G7        |"
+                    "C6        |          | E7             |           |"
+                    "A7        |          | Dm7            |           |"
+                    "F6        | Fm6      | Cmaj7   Em7-5  | A7        |"
+                    "Dm7       | G7       | C6      Ebdim7 | Dm7    G7 |"))}
+  {:id 4, :nm "Autumn Leaves", :numer 4, :denom 4, :ppq 400000, :bb 8, :bpm 120,
+   :bars (bars (str "Am7    | D7    | Gmaj7    | Cmaj7    |"
+                    "F#m7-5 | B7    | Em       | Em       |"
+                    "Am7    | D7    | Gmaj7    | Cmaj7    |"
+                    "F#m7-5 | B7    | Em       | Em       |"
+                    "F#m7-5 | B7    | Em       | Em       |"
+                    "Am7    | D7    | Gmaj7    | Gmaj7    |"
+                    "F#m7-5 | B11-9 | Em7   A7 | Dm7    G7|"
+                    "F#m7-5 | B11-9 | Em       | Em       |"))}
+  {:id 5, :nm "All By Myself", :numer 4, :denom 4, :ppq 400000, :bb 8, :bpm 120,
+   :bars (bars (str "Cmaj7       | C6        | D7             | Am7    D7 |"
+                    "G7          | Dm7   G7  | Em7    A7      | Dm     G7 |"
+                    "Cmaj7       | C6        | F#m7    B7     | E7        |"
+                    "Am7   Am7-5 | D7        | Dm7      Dm7-5 | G7        |"
+                    "Cmaj7       | C6        | D7             | Am7    D7 |"
+                    "G7          | Dm7   G7  | E7    E7+5     | E7        |"
+                    "Fmaj7       | F#dim7    | Cmaj7  B7+5    | Em7-5  A7 |"
+                    "Am7   D7    | Dm7    G7 | C6     Am7     | Dm7    G7 |"))}])
 
-(save-song conn (second songs))
+(for [i (range (count songs))]
+   (save-song conn (nth songs i)))
+
+
 
 ; (-> conn .createStatement (.executeUpdate "create table song (song_id number, song_name string);"))
 ;(batch-update conn "insert into song(song_id, song_nm, time_sig_nmrtr_num, time_sig_denom_num, time_sig_ppq_num, time_sig_bb_num, bpm_num) values (?, ?, ?, ?, ?, ?, ?)"
